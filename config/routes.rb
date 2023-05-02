ceda135c503ba0986988a00b73e574d89ac116be
Sean Cowans <zedesia@yahoo.com>
2023-05-02 15:19:30 -0600

    feat: Create user authentication structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "static_pages#home"
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
+  resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  get "login", to: "sessions#new"
+  resources :passwords, only: [:create, :edit, :new, :update], param: :password_reset_token
+  put "account", to: "users#update"
+  get "account", to: "users#edit"
+  delete "account", to: "users#destroy"
+  resources :active_sessions, only: [:destroy] do
+    collection do
+      delete "destroy_all"
+    end
+  end
 end
