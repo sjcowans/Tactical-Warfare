6862ec1032af1ef35e69c7b148270e74e92b57a1
Sean Cowans <76144522+sjcowans@users.noreply.github.com>
2023-07-10 14:39:09 -0600

    feat: Refactor css

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,6 +70,18 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  # Disable Rails's static asset server (Apache or nginx will already do this)  
+  config.serve_static_assets = false
+
+  # Compress JavaScripts and CSS  
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed  
+  config.assets.compile = false
+
+  # Generate digests for assets URLs  
+  config.assets.digest = true
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
