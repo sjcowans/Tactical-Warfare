4f8f558c8fc9c1dd85c7e31739f8b279b1018243
sjcowans <zedesia@yahoo.com>
2026-02-19 16:16:18 -0700

    Fix mailer redirect host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.force_ssl = true
 
-  config.action_mailer.default_url_options = { host: 'https://tactical-warfare.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://tactical-warfare-a6c93ce8ce20.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
